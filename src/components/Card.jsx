af089fc09973a10875e51ad17e049175e3e01e32
ChikoHakles <2014484.chikopratama@gmail.com>
2023-03-10 08:32:15 +0700

    Refactor: card auto wrap, card x-margin auto, card change style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,15 @@ import "./styles/Card.css";
 
 function Card(props) {
     return (
-        <div className='myCard' key={props.id.toString()}>
-          <h3>{props.nama}</h3>
-          <h5>{props.alamat}</h5>
-          <h5>{props.email}</h5>
-          <h5>{props.telepon}</h5>
+      <div className="card" key={props.id.toString()}>
+        <div class="card-body">
+          <h3 class="card-title">{props.nama}</h3>
+          <p className="card-text">{props.alamat}</p>
+          <p className="card-text">{props.email}</p>
+          <p className="card-text">{props.telepon}</p>
+          <a href="#" class="card-link">Profil</a>
         </div>
+      </div>
     )
 }
 export default Card;
