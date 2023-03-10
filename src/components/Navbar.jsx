ba8fdf7277e7280075be24bd473497974880dec7
ChikoHakles <2014484.chikopratama@gmail.com>
2023-03-10 09:04:42 +0700

    Delete: melon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,16 @@ import './styles/Navbar.css';
 
 function Navbar() {
     return (
-        <>
-            <div className='navbar'>
-                <div className='title-container'>
-                    NGOKNGOK
-                </div>
-                <ul className='menu-container'>
-                    <a href="#"><li className='menu-list'>HOME</li></a>
-                    <a href="#"><li className='menu-list'>ABOUT</li></a>
-                    <a href="#"><li className='menu-list'>CONTACT</li></a>
-                </ul>
+        <div className='navbar'>
+            <div className='title-container'>
+                NGOKNGOK
             </div>
-            <div style={{
-                width : "100vw",
-                height : "100vh",
-                position: "fixed",
-                backgroundImage : "url('https://pbs.twimg.com/profile_images/1363503631312453633/8tDNJiBX_400x400.jpg')",
-                backgroundRepeat: "no-repeat",
-                objectFit : "cover",
-                backgroundSize: "cover",
-                top: "0px"
-            }}>
-
-            </div>
-        </>
+            <ul className='menu-container'>
+                <a href="#"><li className='menu-list'>HOME</li></a>
+                <a href="#"><li className='menu-list'>ABOUT</li></a>
+                <a href="#"><li className='menu-list'>CONTACT</li></a>
+            </ul>
+        </div>
     );
 }
 export default Navbar;
