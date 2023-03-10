c5c4397e392e732b28d0d4a37eb6d65db27dac3f
ChikoHakles <2014484.chikopratama@gmail.com>
2023-03-10 08:43:33 +0700

    Refactor: card min-width, card-link in bottom

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,11 @@ function Card(props) {
       <div className="card" key={props.id.toString()}>
         <div class="card-body">
           <h3 class="card-title">{props.nama}</h3>
-          <p className="card-text">{props.alamat}</p>
-          <p className="card-text">{props.email}</p>
-          <p className="card-text">{props.telepon}</p>
+          <div className="card-content">
+            <p className="card-text">{props.alamat}</p>
+            <p className="card-text">{props.email}</p>
+            <p className="card-text">{props.telepon}</p>
+          </div>
           <a href="#" class="card-link">Profil</a>
         </div>
       </div>
