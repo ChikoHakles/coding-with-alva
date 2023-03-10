f6b8b4e0ea7718aebc9df01b2d42864a97655463
ChikoHakles <2014484.chikopratama@gmail.com>
2023-03-10 09:02:35 +0700

    melon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,30 @@ import './styles/Navbar.css';
 
 function Navbar() {
     return (
-        <div className='navbar'>
-            <div className='title-container'>
-                NGOKNGOK
+        <>
+            <div className='navbar'>
+                <div className='title-container'>
+                    NGOKNGOK
+                </div>
+                <ul className='menu-container'>
+                    <a href="#"><li className='menu-list'>HOME</li></a>
+                    <a href="#"><li className='menu-list'>ABOUT</li></a>
+                    <a href="#"><li className='menu-list'>CONTACT</li></a>
+                </ul>
             </div>
-            <ul className='menu-container'>
-                <a href="#"><li className='menu-list'>HOME</li></a>
-                <a href="#"><li className='menu-list'>ABOUT</li></a>
-                <a href="#"><li className='menu-list'>CONTACT</li></a>
-            </ul>
-        </div>
+            <div style={{
+                width : "100vw",
+                height : "100vh",
+                position: "fixed",
+                backgroundImage : "url('https://pbs.twimg.com/profile_images/1363503631312453633/8tDNJiBX_400x400.jpg')",
+                backgroundRepeat: "no-repeat",
+                objectFit : "cover",
+                backgroundSize: "cover",
+                top: "0px"
+            }}>
+
+            </div>
+        </>
     );
 }
 export default Navbar;
